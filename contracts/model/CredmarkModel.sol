cbcc0620c663b01adf057a1e9b8643222b46fcdc
Neil Zumwalde <neilzumwalde@gmail.com>
2021-10-25 13:13:49 -0500

    Model Changes

--- contracts/model/CredmarkModel.sol
+++ contracts/model/CredmarkModel.sol
@@ -21,7 +21,8 @@ contract CredmarkModel is ERC721, ERC721Enumerable, AccessControl {
 
     enum ValidationStatus {
         SUBMITTED,
-        INCLUDED,
+        ACCEPTED,
+        REJECTED,
         UNDER_INVESTIGATION,
         RETRACTED
     }
@@ -34,20 +35,32 @@ contract CredmarkModel is ERC721, ERC721Enumerable, AccessControl {
     event ModelMinted(uint256 tokenId, uint modelHash);
     event CredmarkAddedToModel(uint256 tokenId, uint256 amount);
 
-    mapping(uint => ModelInfo) private _statuses;
-    mapping(uint256 => uint256) private _sharesLocked;
-    mapping(uint256 => uint256) private _hashToId;
+    mapping (uint256 => ModelInfo) private _infos;
+    mapping (uint256 => uint256) private _sharesLocked;
+    mapping (uint256 => uint256) private _hashToId;
 
-    uint minimumCollateral;
+    uint public mintCollateral;
 
     constructor(IStakedCredmark _stakedCredmark, IERC20 _credmark) ERC721("CredmarkModel", "modelCMK") {
         stakedCredmark = _stakedCredmark;
         credmark = _credmark;
     }
 
-    // User Functions
+    modifier collateralRemovable(uint tokenId) {
+        require(
+            _infos[tokenId].validationStatus == ValidationStatus.ACCEPTED ||
+            _infos[tokenId].validationStatus == ValidationStatus.REJECTED ||
+            _infos[tokenId].validationStatus == ValidationStatus.RETRACTED);
+        _;
+    }
+
+    modifier tokenExists(uint tokenId) {
+        require(_exists(tokenId),"No such token");
+        _;
+    }
+
     function mint(address to, uint256 cmkAmount, uint modelHash) external returns (uint256 tokenId) {
-        require(cmkAmount > minimumCollateral, "Require More CMK to Mint Model");
+        require(cmkAmount > mintCollateral, "Require More CMK to Mint Model");
         require(_hashToId[modelHash] == 0x0, "Non Unique Model Hash");
 
         tokenId = _tokenIdCounter.current();
@@ -55,20 +68,29 @@ contract CredmarkModel is ERC721, ERC721Enumerable, AccessControl {
 
         _safeMint(to, tokenId);
         _hashToId[modelHash] = tokenId;
-        _statuses[tokenId] = ModelInfo(ValidationStatus.SUBMITTED, modelHash);
+        _infos[tokenId] = ModelInfo(ValidationStatus.SUBMITTED, modelHash);
 
         _tokenIdCounter.increment();
 
         emit ModelMinted(tokenId, modelHash);
     }
 
-    function addCmk(uint256 tokenId, uint256 cmkAmount) public  {
-        require(_exists(tokenId),"No such token");
+    function addCmk(uint256 tokenId, uint256 cmkAmount) public tokenExists(tokenId) {
         credmark.transferFrom(msg.sender, address(this), cmkAmount);
         uint256 sCmk = stakedCredmark.createShare(cmkAmount);
         _sharesLocked[tokenId] += sCmk;
 
         emit CredmarkAddedToModel(tokenId, cmkAmount);
+    }
+
+    function removeCollateral(uint tokenId, uint256 cmkAmount) external tokenExists(tokenId) collateralRemovable(tokenId) {
+        
+        uint shares = stakedCredmark.cmkToShares(cmkAmount);
+        require(shares >= _sharesLocked[tokenId]);
+        stakedCredmark.removeShare(shares);
+
+        //cmkAmount may be different than removed shares if calls rewards
+        credmark.transfer(ownerOf(tokenId), cmkAmount);
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
